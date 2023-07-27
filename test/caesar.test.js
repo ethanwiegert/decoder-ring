9d8274784b32b3abc04c9c56a892a3beb4ecd15b
Ethan Wiegert <ewiegert99@gmail.com>
2023-07-27 11:55:54 -0400

    Written tests to check that the caesar shift functions properly.

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -7,7 +7,72 @@
 -also check encode/decode
 
 use examples */
-//use cesar("Zebra Magazine", 3)= "cheud pdjdclqh"
+//use caesar("Zebra Magazine", 3)= "cheud pdjdclqh"
+const {expect}=require("chai")
+const {caesar}=require("../src/caesar")
+
+describe("caesar() tests", ()=>{
+    describe("caesar() checks inputs", ()=>{
+        it("returns false for shift 0", ()=>{
+            const actual=caesar("encoded message", 0)
+            expect(actual).to.be.false
+        })
+        it("returns false for shift less than -25", ()=>{
+            const actual=caesar("encoded message", -27)
+            expect(actual).to.be.false
+        })
+        it ("returns false for shift above 25", ()=>{
+            const actual=caesar("encoded message", 28)
+            expect(actual).to.be.false
+        })
+
+    })
+
+    describe("caesar() output for encoding", ()=>{
+        it("returns encoded message, with negative shift", ()=>{
+            const expected="wxa"
+            const actual=caesar("abe", -4)
+            expect(actual).to.equal(expected)
+        })
+        it("spaces and characters maintained", ()=>{
+            const expected="amkzmb umaaiom"
+            const actual=caesar("secret message", 8)
+            expect(actual).to.equal(expected)
+        })
+        it ("capital letters ignored", ()=>{
+            const expected="mjqqt"
+            const actual=caesar("HELLO", 5)
+            expect(actual).to.equal(expected)
+        })
+        it ("shifts off alphabet work properly", ()=>{
+            const expected="cheud"
+            const actual=caesar("Zebra", 3)
+            expect(actual).to.equal(expected)
+        })
+    })
+    describe("caesar() output for decoding", ()=>{
+        it("returns encoded message, with negative shift", ()=>{
+            const expected="abe"
+            const actual=caesar("wxa", -4, false)
+            expect(actual).to.equal(expected)
+        })
+        it("spaces and characters maintained", ()=>{
+            const expected="secret message"
+            const actual=caesar("amkzmb umaaiom", 8, false)
+            expect(actual).to.equal(expected)
+        })
+        it ("capital letters ignored", ()=>{
+            const expected="hello"
+            const actual=caesar("mjQqT", 5, false)
+            expect(actual).to.equal(expected)
+        })
+        it ("shifts off alphabet work properly", ()=>{
+            const expected="zebra"
+            const actual=caesar("cheud", 3, false)
+            expect(actual).to.equal(expected)
+        })
+    })
 
 
+})
 
